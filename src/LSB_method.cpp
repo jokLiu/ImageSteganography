b428c02d42f57289359f12bac6bce2889b409ae8
Jokubas Liutkus <liutkusjokubas@gmail.com>
2017-12-25 23:07:21 +0000

    LSB method comments sorted

--- src/LSB_method.cpp
+++ src/LSB_method.cpp
@@ -1,6 +1,27 @@
+//===----------------------------------------------------------------------===//
 //
-// Created by jokubas on 12/12/17.
+//                           The MIT License (MIT)
+//                    Copyright (c) 2017 Jokubas Liutkus
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//===----------------------------------------------------------------------===//
+
 #include <string>
 #include <assert.h>
 #include <iostream>
@@ -26,61 +47,66 @@ namespace steg {
     char LSB_decode_single_byte(CImg<unsigned char> &image,
                                 std::pair<int, int> &coord);
 
-    // first 64 pixels encodes the length of the message in bytes
-    // message has to be of at least 64x64 size
-    void StegCoding::LSB_encode(std::string name, std::string message) {
+
+    void StegCoding::LSB_encode(const std::string& name, const std::string& message) {
         CImg<unsigned char> src(name.c_str());
         uint64_t msg_length = message.length();
 
-        assert(src.width() >= 64 &&
-               src.height() >= 64 &&
-               src.width() * src.height() / BIT_TO_BYTE >=
-               ENCODE_SIZE + msg_length);
+        assert(src.width() >= 64 &&  // width and height of correct size
+               src.height() >= 64);
 
         // encode length of the text
         LSB_enocde_length(msg_length, src);
 
-        // encoding the message
+        // encoding the actual message
         auto coord = std::make_pair(ENCODE_SIZE, 0);
         for (char c : message) {
             coord = LSB_encode_single_byte(c, src, coord);
         }
 
+        // save the encoded image
         src.save(name.c_str());
     }
 
-    std::string StegCoding::LSB_decode(std::string name) {
+    std::string StegCoding::LSB_decode(const std::string& name){
         CImg<unsigned char> src(name.c_str());
         std::string message = "";
         uint64_t msg_length = 0;
 
         // decode length and translate it to bits
-        msg_length = LSB_decode_length(src) * BIT_TO_BYTE + ENCODE_SIZE;
+        msg_length = LSB_decode_length(src) * BIT_TO_BYTE 
+                                            + ENCODE_SIZE; 
 
         // decode message
-        int height = src.height();
         int width = src.width();
-        int total_pixels = width * height;
-        char t;
+        int total_pixels = width * src.height();
+        // coordinates to pass into the decode single byte
         auto coord = std::make_pair(ENCODE_SIZE, 0);
-        for (int nr = ENCODE_SIZE; nr < total_pixels && nr < msg_length; nr += BIT_TO_BYTE) {
+        for (int nr = ENCODE_SIZE; nr < total_pixels && nr < msg_length; 
+                nr += BIT_TO_BYTE) {
+            // determine the the pixel coordinates
             coord.first = nr % width; // width
             coord.second = nr / width; //height
-            t = LSB_decode_single_byte(src, coord);
-            message += t;
+            message += LSB_decode_single_byte(src, coord);
+            
         }
         return message;
     }
 
     char LSB_decode_single_byte(CImg<unsigned char> &image,
                                 std::pair<int, int> &coord) {
-        int bit = 0;
+        // bit to be retrieved from a single pixel
+        int bit = 0; 
+        // single byte to decode
         uint8_t to_decode = 0;
+        // width pixel
         int w = coord.first;
         int count = 0;
         for (int h = coord.second; h < image.height(); h++) {
             for (; w < image.width() && count < BIT_TO_BYTE; w++, count++) {
+                // retrieve a single bit
                 bit = image(w, h, BLUE) & 1U;
+                // update the byte
                 to_decode <<= 1;
                 to_decode ^= (-bit ^ to_decode) & 1U;
             }
@@ -94,7 +120,10 @@ namespace steg {
         int bit;
         for (int i = 0; i < ENCODE_SIZE; i++) {
             msg_length <<= 1;
+            // get the bit at the specific pixel
             bit = image(i, 0, BLUE) & 1U;
+
+            // update the length
             msg_length ^= (-bit ^ msg_length) & 1U;
         }
         return msg_length;
@@ -108,8 +137,9 @@ namespace steg {
         int bit;
         // encoding the size to the picture
         for (int i = ENCODE_SIZE - 1, j = 0; i >= 0; i--, j++) {
+            // get the bit at the position
+            bit = (msg_length >> j) & 1U; 
 
-            bit = (msg_length >> j) & 1U; // get the bit at the position
             // changing blue value of the message
             image(i, 0, BLUE) ^= (-bit ^ image(i, 0, BLUE)) & 1U;
         }
--- src/steganography.h
+++ src/steganography.h
@@ -1,6 +1,26 @@
+//===----------------------------------------------------------------------===//
 //
-// Created by jokubas on 11/12/17.
+//                           The MIT License (MIT)
+//                    Copyright (c) 2017 Jokubas Liutkus
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//===----------------------------------------------------------------------===//
 
 #ifndef IMAGE_STEGANOGRPAHY_STEGANOGRAPHY_H
 #define IMAGE_STEGANOGRPAHY_STEGANOGRAPHY_H
@@ -18,48 +38,79 @@ namespace steg {
     class StegCoding {
     public:
 
-        // Encodes the image of the given name (and path to the image)
-        // using the Least Significant Bit (LSB) method where each pixel
-        // in the image is modified and it's least significant bit is
-        // replaced with the the one of the given message which has to
-        // be hidden.
-        // The example of the possible encoding:
-        // Given grey image with the values between 0 to 255:
-        //
-        //          200 198 0
-        //          100 15  10
-        //           99 12  5
-        //
-        // We want to encode the letter L which is 76 in decimal
-        // this yields 01001100 in binary.
-        //
-        // We are going to encode each bit starting from the top left
-        // and working our way to the right, and then down the rows
-        // i.e 200 -> 198 -> 0 -> 100 -> 15 -> 10 -> 99 -> 12 -> 5
-        //
-        // First we write our picture's pixels in binary format:
-        //
-        //       11001000  11000110  00000000
-        //       01100100  00001111  00001010
-        //       01100011  00001100  00000101
-        //
-        // Now we are going to encrypt our message [0]1001100
-        // First bit in the message is 0 and the first pixel is
-        // 11001000 we replace the LSB of the pixel this yields the
-        // 1100100[0] pixel with encoded bit in the brackets
-        //
-        // replacing the second bit in the message 0[1]001100
-        // this yields the second pixel 11000110 to become:
-        // 1100011[1], and so on, until we get the image:
-        //
-        //       11001000  11000110  00000000
-        //       01100100  00001111  00001010
-        //       01100011  00001100  00000101
-        //
-        static void LSB_encode(std::string name, std::string message);
+        /************************************************
+         * Encodes the message into the image which name is passed to 
+         * the function using the Least Significant Bit (LSB) method 
+         * where each pixel in the image is modified and it's least 
+         * significant bit is replaced with the the one of the given 
+         * message which has to be hidden. Then it replaces the actual
+         * image with the new stego image with the message hidden in it.
+         *
+         * Image to encode has to be of the "png" file format, and
+         * all the data is at the moment hidden in the BLUE pixels
+         * as it is the least sensitive to the human eye.
+         *
+         * First of all the length of the message is encoded into first
+         * 64 pixels, this provides the max message size of 2^64 which
+         * is very unlikely to be exceeded (heh).
+         *
+         * At the moment image has to be of at least 64x64 size.
+         *
+         **************************************************
+         *
+         * The example of the possible encoding:
+         * Given the image with the BLUE values between 0 to 255:
+         *
+         *          200 198 0
+         *          100 15  10
+         *           99 12  5
+         *
+         * We want to encode the letter L which is 76 in ASCII
+         * this yields 01001100 in binary.
+         *
+         * We are going to encode each bit starting from the top left
+         * and working our way to the right, and then down the rows
+         * i.e 200 -> 198 -> 0 -> 100 -> 15 -> 10 -> 99 -> 12 -> 5
+         *
+         * First we write our picture's pixels in binary format:
+         *
+         *       11001000  11000110  00000000
+         *       01100100  00001111  00001010
+         *       01100011  00001100  00000101
+         *
+         * Now we are going to encrypt our message bit by bit:
+         *
+         *                   [0]1001100
+         *
+         * First bit in the message is [0] and the first pixel is
+         * 11001000 we replace the LSB of the pixel this yields the
+         * 1100100[0] pixel with encoded bit in the brackets
+         *
+         * replacing the second bit in the message 0[1]001100
+         * yields the second pixel 11000110 to become:
+         * 1100011[1], and so on, until we get the image:
+         *
+         *       1100100[0]  1100011[1]  0000000[0]
+         *       0110010[0]  0000111[1]  0000101[1]
+         *       0110001[0]  0000110[0]  00000101
+         ***********************************************/
+        static void LSB_encode(const std::string& name, const std::string& message);
 
 
-        static std::string LSB_decode(std::string name);
+        /************************************************
+         * Decodes the message from the image which name is passed to 
+         * the function using the Least Significant Bit (LSB) method.
+         * This is the opposite of LSB_encode, and the message encoded 
+         * with LSB_encoded can be retrieved using this LSB_decode method.
+         *
+         * The message is retrieved from the image and returned as a string.
+         * 
+         * If random image is passed the are no checks done whether the
+         * encoding was used, and therefore it is impossible to determine
+         * if the LSB method was used or not.
+         *         
+         ***********************************************/
+        static std::string LSB_decode(const std::string& name);
 
 
         static void LSB_encode_odd(std::string name, std::string message);
